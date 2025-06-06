d5cebfdf87ff9fb07d718ac06873eaf245d474e9
mauri <mauri@localhost.localdomain>
2025-06-06 01:21:13 -0400

    icon

--- server-app/resources/js/components/app-logo-icon.tsx
+++ server-app/resources/js/components/app-logo-icon.tsx
@@ -3,7 +3,7 @@ export default function AppLogoIcon({organization} ){
     return (
         <>
             {organization && <img src={`http://localhost:8000/storage/${organization.file.path}`} alt='Logo' />}
-            {!organization && <img src="http://localhost:8000/storage/logo-img.png" alt='Logo' />}
+            {!organization && <img src="http://localhost:8000/logo-img.png" alt='Logo' />}
         </>
     );
 }
