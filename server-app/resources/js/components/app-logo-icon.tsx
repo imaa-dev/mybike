8f72a19cbb04993abd5b9846cbb261ce80666f16
mauri <mauri@localhost.localdomain>
2025-06-09 10:11:11 -0400

    test

--- server-app/resources/js/components/app-logo-icon.tsx
+++ server-app/resources/js/components/app-logo-icon.tsx
@@ -1,5 +1,6 @@
 const appUrl = import.meta.env.VITE_APP_URL;
 export default function AppLogoIcon({organization} ){
+
     return (
         <>
             {organization && <img src={`${appUrl}/storage/${organization.file.path}`} alt='Logo' />}
--- server-app/resources/js/lib/utils.ts
+++ server-app/resources/js/lib/utils.ts
@@ -1,6 +1,25 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import imageCompression from 'browser-image-compression';
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export async function handleImageUpload(event){
+    const imageFiles = event.target.files;
+    console.log(imageFiles);
+    const options = {
+        maxSizeMB: 0.5,
+        maxWidthOrHeight: 800,
+        useWebWorker: true,
+    }
+    try {
+        const compressedImage = await imageCompression(imageFiles[0], options)
+        return compressedImage;
+    } catch (error) {
+        console.log(error)
+
+    }
+}
+
