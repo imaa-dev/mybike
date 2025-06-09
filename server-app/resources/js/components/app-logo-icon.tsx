e7fe8c9a65afa5d5c2f554650a29a7b68714ef71
mauri <mauri@localhost.localdomain>
2025-06-09 10:18:50 -0400

    testing

--- server-app/resources/js/components/app-logo-icon.tsx
+++ server-app/resources/js/components/app-logo-icon.tsx
@@ -1,9 +1,9 @@
 const appUrl = import.meta.env.VITE_APP_URL;
 export default function AppLogoIcon({organization} ){
-
+    console.log(organization, 'APPLOGOICON')
     return (
         <>
-            {organization && <img src={`${appUrl}/storage/${organization.file.path}`} alt='Logo' />}
+            {organization && organization.file.path && <img src={`${appUrl}/storage/${organization.file.path}`} alt='Logo' />}
             {!organization && <img src={`${appUrl}/logo-img.png`} alt='Logo' />}
         </>
     );
