c70d75857399f198876337cca4d3581a5398418a
mauri <mauri@localhost.localdomain>
2025-06-11 21:57:19 -0400

    front decoration

--- server-app/resources/js/pages/product.tsx
+++ server-app/resources/js/pages/product.tsx
@@ -41,67 +41,15 @@ export default function Product({products}: ProductDataProp){
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Productos" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+                <div className="relative">
+                    <button type="button" className="flex" onClick={() => router.visit('/create/product')}  >
+                        <CirclePlus />
+                        Agregar Producto
+                    </button>
+                </div>
                 <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min">
                     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                        <div className="flex items-center justify-between flex-column md:flex-row flex-wrap space-y-4 md:space-y-0 py-4 bg-white dark:bg-gray-900">
-                            <div>
-                                <button id="dropdownActionButton" data-dropdown-toggle="dropdownAction"
-                                        className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-                                        type="button">
-                                    <span className="sr-only">Action button</span>
-                                    Action
-                                    <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true"
-                                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                        <path  d="m1 1 4 4 4-4" />
-                                    </svg>
-                                </button>
-                                <div id="dropdownAction"
-                                     className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600">
-                                    <ul className="py-1 text-sm text-gray-700 dark:text-gray-200"
-                                        aria-labelledby="dropdownActionButton">
-                                        <li>
-                                            <a href="#"
-                                               className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Reward</a>
-                                        </li>
-                                        <li>
-                                            <a href="#"
-                                               className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Promote</a>
-                                        </li>
-                                        <li>
-                                            <a href="#"
-                                               className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Activate
-                                                account</a>
-                                        </li>
-                                    </ul>
-                                    <div className="py-1">
-                                        <a href="#"
-                                           className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete
-                                            User</a>
-                                    </div>
 
-                                </div>
-                            </div>
-                            <div className="relative">
-                                <button type="button" className="flex" onClick={() => router.visit('/create/product')}  >
-                                    <CirclePlus />
-                                    Agregar Producto
-                                </button>
-                            </div>
-                            <label htmlFor="table-search" className="sr-only">Search</label>
-                            <div className="relative">
-                                <div
-                                    className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
-                                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                        <path d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                                    </svg>
-                                </div>
-                                <input type="text" id="table-search-users"
-                                       className="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                       placeholder="Search for users" />
-                            </div>
-
-                        </div>
                         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                             <thead
                                 className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
